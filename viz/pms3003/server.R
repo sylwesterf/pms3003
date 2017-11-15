0694b28924bf46d9cadeff412c4d5e89cdb7a66a
Sylwester <sylwesterf@users.noreply.github.com>
2017-11-16 00:50:23 +0100

    ui changes, move conn var outside of the plot def

--- viz/pms3003/server.R
+++ viz/pms3003/server.R
@@ -6,26 +6,26 @@ library(plotly)
 # set up a server variable
 server <- function(input, output, session) {
   
-  # define output
-  output$plot <- renderPlotly({
+  # set up mysql connectivity
+  conn <- dbConnect(
+    drv = RMySQL::MySQL(),
+    host='xxx',
+    user='xxx',
+    password='xxx',
+    dbname='xxx',
+    port=3306)
+  on.exit(dbDisconnect(conn), add = TRUE)
   
-    # set up mysql connectivity
-    conn <- dbConnect(
-      drv = RMySQL::MySQL(),
-      host='xxx',
-      user='xxx',
-      password='xxx',
-      dbname='xxx',
-      port=3306)
-    on.exit(dbDisconnect(conn), add = TRUE)
-	
-	# produce a query
-    query <- paste0("select dt, pm1, pm25, pm10 from fct_pm")
-	
-	# store retrieved data
-    data <- dbGetQuery(conn, query)
-	
-	# render a plot
+  # produce a query
+  query <- paste0("select dt, pm1, pm25, pm10 from fct_pm")
+  
+  # store retrieved data
+  data <- dbGetQuery(conn, query)
+  
+  # define output object
+  output$plot <- renderPlotly({
+    
+    # render a plot
     plot_ly(data,
             type = "scatter",
             mode = "lines", 
@@ -34,21 +34,21 @@ server <- function(input, output, session) {
             name = "PM1"
     ) %>%
       
-	  # add pm2.5 line
+      # add pm2.5 line
       add_trace(x = ~dt,                                         
                 y = ~pm25,  
                 mode = "lines",
                 name = "PM2,5"
       ) %>%
       
-	  # add pm10 line
+      # add pm10 line
       add_trace(x = ~dt,                                         
                 y = ~pm10,  
                 mode = 'lines', 
                 name = "PM10"
       ) %>%
       
-	  # format titles
+      # format titles
       layout(                  
         title = "Zanieczyszczenie powietrza",
         xaxis = list(         
@@ -56,7 +56,12 @@ server <- function(input, output, session) {
           type = "date"
         ),      
         yaxis = list(         
-          title = "μg/m³")   
+          title = "µg/m3")   
       )
   })
+  
+  # print latest data
+  output$latest.data <- renderText({ paste("Ostatni pomiar wykonano ", 
+                                           tail(data, 1)[1,1])})
+  
 }
