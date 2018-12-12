110466a0b7a02bd6d747eefdb1b302c8cc055805
Sylwester <sylwesterf@users.noreply.github.com>
2018-12-12 14:03:12 +0100

    Update viz-python.py

--- viz/viz-python.py
+++ viz/viz-python.py
@@ -1,58 +1,108 @@
+# -*- coding: utf-8 -*-
 #!/bin/python
 import time
 import pandas as pd
 import boto3
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Event
+import plotly
+import plotly.graph_objs as go
 
-# select statement
+# config boto3 first
+# get the service resource
 dynamodb = boto3.resource('dynamodb')
+
+# instantiate a table resource object 
 table = dynamodb.Table('pms3003')
 
-response = table.scan()
-data = response['Items']
+# initiate a dash app
+app = dash.Dash()
 
-while 'LastEvaluatedKey' in response:
-    response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-    data.extend(response['Items'])
+# set formatting
+colors = {
+    'background': '#ffffff',
+    'text': '#111101'
+}
 
-df = pd.DataFrame(data, columns=['dt','pm1','pm25','pm10']).sort_values('dt').set_index('dt').astype(int)
-df.index = pd.to_datetime(df.index)
+# actual dash app layout definition
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
 
-df.plot()
+	# title
+    html.H1(
+        children='Zanieczyszczenie powietrza',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+	
+	# graph
+    dcc.Graph(
+        id='live-graph',
+		animate = True
+    ),
+	
+	# graph update handler
+	dcc.Interval(
+            id='graph-update',
+            interval=1*1000
+        ),
+		
+	# last updated note
+	html.Div(children='Ostatni pomiar wykonano ' + str(1), style={
+        'textAlign': 'right',
+        'color': colors['text'],
+		'fontSize': 12
+    })
+])
 
-#2
+# app callback for graph update
+@app.callback(Output('live-graph', 'figure'),
+              events=[Event('graph-update', 'interval')])
+			  
+# function for the graph udpate
+def update_graph():
 
-import plotly.plotly as py
-import plotly.graph_objs as go
+	# scan the table
+	response = table.scan()
+	data = response['Items']
+	
+	# create a pandas dataframe, wrangle the data
+	df = pd.DataFrame(data, columns=['dt','pm1','pm25','pm10']).sort_values('dt').set_index('dt').astype(int)
+	df.index = pd.to_datetime(df.index)
 
-trace0 = go.Scatter(
-    x=df.index,
-    y=df['pm1'],
-    name='pm1'
-)
-trace1 = go.Scatter(
-    x=df.index,
-    y=df['pm25'],
-    name='pm2.5'
-)
-trace2 = go.Scatter(
-    x=df.index,
-    y=df['pm10'],
-    name='pm10'
-)
+	# trace0 - pm1
+	trace0 = plotly.graph_objs.Scatter(
+		x=df.index,
+		y=df['pm1'],
+		name='pm1',
+		mode= 'lines+markers'
+	)
+	
+	# trace1 - pm2.5
+	trace1 = plotly.graph_objs.Scatter(
+		x=df.index,
+		y=df['pm25'],
+		name='pm2.5',
+		mode= 'lines+markers'
+	)
+	
+	# trace2 - pm10
+	trace2 = plotly.graph_objs.Scatter(
+		x=df.index,
+		y=df['pm10'],
+		name='pm10',
+		mode= 'lines+markers'
+	)
+	
+	# combine lines
+	data = [trace0, trace1, trace2]
 
-data = [trace0, trace1, trace2]
+	# return quasi-live data for graph's figure attribute
+	return {'data': data,'layout' : go.Layout(yaxis = dict(title = "µg/m3"))}
 
-import plotly
-plotly.tools.set_credentials_file(username='xyz', api_key='xyz')
-py.iplot(data)
-
-#3
-import plotly
-import plotly.graph_objs as go
-
-plotly.offline.init_notebook_mode(connected=True)
-
-plotly.offline.iplot({
-    "data": data,
-    "layout": go.Layout(title="Zanieczyszczenie powietrza", yaxis = dict(title = "µg/m3"))
-})
+# run
+if __name__ == '__main__':
+    app.run_server(debug=True)
