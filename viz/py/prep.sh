4cc355bfcc42ae15f02d899cc8bafae5979e7760
Sylwester <sylwesterf@users.noreply.github.com>
2018-12-23 12:47:45 +0100

    Update prep.sh

--- viz/py/prep.sh
+++ viz/py/prep.sh
@@ -26,8 +26,5 @@ git clone https://github.com/sylwesterf/pms3003.git
 # copy viz files
 cp pms3003/viz/py/{fun.py,requirements.txt,vizflask.py,wsgi.py} .
 
-# run activate script
-source ./flask/bin/activate
-
 # activate script flask venv, install flask app requirements and run wsgi server
 /bin/bash -c ". /opt/pms3003/flaskr/bin/activate; pip install -r requirements.txt; gunicorn --bind 0.0.0.0:80 wsgi:server &"
