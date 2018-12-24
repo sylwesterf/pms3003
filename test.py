826ad927b747d62ae79436c75d827ad010b76920
Sylwester <sylwesterf@users.noreply.github.com>
2018-12-24 12:38:42 +0100

    Update test.py

--- test.py
+++ test.py
@@ -7,5 +7,10 @@ from pms3003 import PMSensor
 # 0 for indoor sensing, 1 for outdoor
 pm = PMSensor(1)
 	
-# print PM1, PM2.5, PM10 values
+# print avg'ed PM1, PM2.5, PM10 values
 print(pm.read_pm())
+
+# wakeup pms3003 (if necessary) and print a single read
+#pm.write_serial('BM\xe4\x00\x01\x01t', 45)
+#pm.write_serial('BM\xe1\x00\x01\x01q', 15)
+#print(pm.single_read())
