009f7c587e601442b79c64f641eac70baa72c875
Sylwester <sylwesterf@users.noreply.github.com>
2019-01-10 00:40:13 +0100

    Update db.sql

--- mysql/db.sql
+++ mysql/db.sql
@@ -6,11 +6,11 @@ pm10 int NOT NULL,
 dt datetime NOT NULL
 );
 
-CREATE USER 'xxx' IDENTIFIED BY 'xxx';
-GRANT ALL ON * TO 'user';
+CREATE USER 'sa' IDENTIFIED BY 'xxx';
+GRANT ALL ON * TO 'sa';
 
-CREATE USER 'xxx' IDENTIFIED BY 'xxx';
+CREATE USER 'shiny' IDENTIFIED BY 'xxx';
 GRANT SELECT ON db_pms3003.fct_pm TO 'shiny';
 
-CREATE USER 'xxx' IDENTIFIED BY 'xxx';
+CREATE USER 'rpi' IDENTIFIED BY 'xxx';
 GRANT INSERT ON db_pms3003.fct_pm TO 'rpi';
