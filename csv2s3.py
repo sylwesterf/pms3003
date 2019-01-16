c396c9981097f759ca4a283361f9f64cc38c72f0
Sylwester <sylwesterf@users.noreply.github.com>
2019-01-16 17:55:16 +0100

    Create csv2s3.py

---
+++ csv2s3.py
@@ -0,0 +1,23 @@
+#!/bin/python
+import boto3
+import time
+import os
+
+# varaibles for upload
+s3filename = time.strftime('%Y/%m/%d.csv')
+s3bucket = 's3bucket'
+filename = '/path/to/csv/file/pm-archive.csv'
+
+try:
+	
+	# create an S3 client
+	s3 = boto3.client('s3')
+	
+	# upload today's archive csv
+	s3.upload_file(filename, s3bucket, s3filename)
+	
+	# remove archive csv from host
+	os.remove(filename)
+	
+except Exception:
+	pass
