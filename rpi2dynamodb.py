a09e77b40b8ac3271c96b34c43044f44de9b84ea
Sylwester <sylwesterf@users.noreply.github.com>
2019-01-21 14:17:38 +0100

    Update rpi2dynamodb.py

--- rpi2dynamodb.py
+++ rpi2dynamodb.py
@@ -2,6 +2,7 @@
 import time
 import boto3
 import csv
+import numpy as np
 from pms3003 import PMSensor
 
 # first run aws configure and set AWS Access Key ID and AWS Secret Access Key
@@ -13,7 +14,15 @@ from pms3003 import PMSensor
 pm = PMSensor(1)
 	
 # get PM1, PM2.5, PM10 values
-pm1, pm25, pm10 = pm.read_pm()
+data = pm.read_pm()
+#pm1, pm25, pm10 = pm.read_pm()
+
+# reject outliers
+data = data[np.all(np.abs((data - np.mean(data, axis=0))) < 2 * np.std(data, axis=0), axis=1)]
+data = data[np.array(data[:,2], dtype=float)/np.array(data[:,1], dtype=float) < 2]
+
+# get the average as an int
+pm1, pm25, pm10 = np.mean(data, axis=0, dtype=int)
 
 # get time
 pm_date = (time.strftime('%Y-%m-%d ') + time.strftime('%H:%M:%S'))
@@ -26,6 +35,7 @@ with open('/path/to/csv/file/pm-archive.csv','a+') as f:
 # write to dynamodb table
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('pms3003')
+table_raw = dynamodb.Table('pms3003raw')
 
 try:
 	# insert the data
@@ -37,6 +47,17 @@ try:
 		'pm10' : pm10
 	    }
 	)
+	
+	# insert raw data
+	for i in range(data.shape[0]):
+		table_raw.put_item(
+			Item={
+			'dt' : pm_date,
+			'pm1' : data[i,0],
+			'pm25' : data[i,1],
+			'pm10' : data[i,2],
+		    }
+		)
 	
 except Exception:
 	# write to csv in case of error
