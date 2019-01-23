6826317f06d54b028c2efca68d9de6e31c0250e9
Sylwester <sylwesterf@users.noreply.github.com>
2019-01-23 02:22:19 +0100

    Update vizflask.py

--- viz/py/vizflask.py
+++ viz/py/vizflask.py
@@ -89,7 +89,7 @@ def serve_layout():
         }),
 	    
 	# latest results pm10
-        html.Div(id='update-pm10', children = generate_graph(table)['lastpm']['pm10'], style={
+        html.Div(id='update-pm10', children = 'PM10: ' + str(generate_graph(table)['lastpm']['pm10']), style={
             'textAlign': 'left',
             'color': colors['text'],
             'fontSize': 100,
@@ -98,7 +98,7 @@ def serve_layout():
         }),
 	    
 	# latest results pm25
-        html.Div(id='update-pm25', children = generate_graph(table)['lastpm']['pm25'], style={
+        html.Div(id='update-pm25', children = 'PM25: ' + str(generate_graph(table)['lastpm']['pm25']), style={
             'textAlign': 'left',
             'color': colors['text'],
             'fontSize': 100,
