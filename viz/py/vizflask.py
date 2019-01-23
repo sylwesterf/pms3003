dc54295b0e780363689fc9a0c87dc8cb5ae3f59b
Sylwester <sylwesterf@users.noreply.github.com>
2019-01-23 01:41:00 +0100

    Update vizflask.py

--- viz/py/vizflask.py
+++ viz/py/vizflask.py
@@ -49,7 +49,35 @@ def serve_layout():
             animate = False,
             figure = go.Figure(
                 data = generate_graph(table)['data'],
-                layout = go.Layout(yaxis = dict(title = "µg/m3"))
+                layout = go.Layout(yaxis = dict(title = "µg/m3"),
+				  xaxis=dict(
+					rangeselector=dict(
+					    buttons=list([
+						dict(count=1,
+						     label='1m',
+						     step='month',
+						     stepmode='backward'),
+						dict(count=6,
+						     label='6m',
+						     step='month',
+						     stepmode='backward'),
+						dict(count=1,
+						    label='YTD',
+						    step='year',
+						    stepmode='todate'),
+						dict(count=1,
+						    label='1y',
+						    step='year',
+						    stepmode='backward'),
+						dict(step='all')
+					    ])
+					),
+					rangeslider=dict(
+					    visible = True
+					),
+					type='date'
+				 )
+				  )
             )
         ),
             
