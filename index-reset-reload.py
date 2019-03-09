b3704ff31ab60be8d3d92fd619c3281264f42396
Sylwester <sylwesterf@users.noreply.github.com>
2019-03-09 12:06:06 +0100

    Update index-reset-reload.py

--- index-reset-reload.py
+++ index-reset-reload.py
@@ -9,11 +9,11 @@ table_js = dynamodb.Table('pms3003js')
 
 # function for scan
 def dynamo_scan(table):
- response = table.scan(table)
- data = response['Items']
- df = pd.DataFrame(data, columns=['dt','pm1','pm25','pm10']).sort_values('dt').set_index('dt').astype(int)
- df.index = pd.to_datetime(df.index)
- return df
+	response = table.scan(table)
+	data = response['Items']
+	df = pd.DataFrame(data, columns=['dt','pm1','pm25','pm10']).sort_values('dt').set_index('dt').astype(int)
+	df.index = pd.to_datetime(df.index)
+	return df
 
 # scan the table
 df = dynamo_scan(table)
@@ -22,7 +22,7 @@ df = dynamo_scan(table)
 for i in range(df.shape[0]):
         table_js.put_item(
                 Item={
-				'device' : 'pms3003',
+		'device' : 'pms3003',
                 'dt' : str(df.index[i]),
                 'pm1' : df.pm1[i],
                 'pm25' : df.pm25[i],
