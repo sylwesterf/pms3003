1f5340b1456548d71010402624b6a4220368ad0c
Sylwester <sylwesterf@users.noreply.github.com>
2019-03-23 23:39:59 +0100

    Update test.py

--- test.py
+++ test.py
@@ -22,11 +22,14 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# read dth11 data using pin 7
-instance = dht11.DHT11(pin = 7)
-result = instance.read()
-temp, hum = result.temperature, result.humidity
-
-# print data
-print(temp)
-print(hum)
+# read dth11 data using pin 4
+instance = dht11.DHT11(pin = 4)
+while True:
+    result = instance.read()
+    if result.is_valid():
+        print("Last valid input: " + str(datetime.datetime.now()))
+        print("Temperature: %d C" % result.temperature)
+        print("Humidity: %d %%" % result.humidity)
+	break
+	
+GPIO.cleanup()
