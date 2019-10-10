058e812aa83916efa81b5093ee4086f2e5ee02bb
sfronczak <sfronczak@microstrategy.com>
2019-10-09 22:19:39 -0400

    Update wsgi.py

--- viz/py-new/wsgi.py
+++ viz/py-new/wsgi.py
@@ -3,10 +3,10 @@
 from werkzeug.wsgi import DispatcherMiddleware
 from pms3003 import pms3003
 
-from latest_pms3003 import app as app1 
-from all_pms3003 import app as app2
+from latest import app as app1 
+from all import app as app2
 
 application = DispatcherMiddleware(pms3003, {
-    '/latest_pms3003': app1.server,
-    '/all_pms3003': app2.server,
+    '/latest': app1.server,
+    '/all': app2.server,
 })
